3b68da45de88ca62b7e0fefec570d0f08645239f
Jarrod Roberson <jarrod@vertigrated.com>
2020-09-20 20:25:20 -0400

    removing redundant code

--- encoding/bais/bais.go
+++ encoding/bais/bais.go
@@ -1,12 +1,12 @@
 // <summary>Encodes and decodes BAIS (Byte Array In String) encoding,
 // which preserves runs of ASCII characters unchanged. This encoding is
-// useful for debugging (since ASCII runs are visible) and for conversion 
+// useful for debugging (since ASCII runs are visible) and for conversion
 // of bytes to JSON.</summary>
 // <remarks>
 // Arrays encoded with <see cref="ByteArrayInString.Convert(ArraySlice{byte}, bool)"/>
-// tend to be slightly more compact than standard Uuencoding or Base64, 
-// and when you use this encoding in JSON with UTF-8, the output is 
-// typically also more compact than yEnc since double-byte characters 
+// tend to be slightly more compact than standard Uuencoding or Base64,
+// and when you use this encoding in JSON with UTF-8, the output is
+// typically also more compact than yEnc since double-byte characters
 // above 127 are avoided.
 // <para/>
 // A BAIS string alternates between runs of "direct" bytes (usually bytes
@@ -17,7 +17,7 @@
 // and has an interesting property described below.
 // <para/>
 // A BAIS string begins in ASCII mode and switches to base 64 when the '\b'
-// character is encountered. Base-64 mode ends, returning to ASCII, when a 
+// character is encountered. Base-64 mode ends, returning to ASCII, when a
 // '!' character is encountered.
 // <para/>
 // For example:
@@ -26,11 +26,11 @@
 //   var b = new byte[] { 67, 97, 116, 128, 10, 69, 255, 65, 66, 67, 68 };
 //   Assert.AreEqual(ByteArrayInString.Convert(b), "Cat\b`@iE?tEB!CD");
 // </pre>
-// A byte sequence such as 128, 10, 69, 255 can be encoded in base 64 as 
+// A byte sequence such as 128, 10, 69, 255 can be encoded in base 64 as
 // illustrated:
 // <pre>
-//              ---128---    ---10----    ---69----  ---255---  
-//   Bytes:     1000 0000    0000 1010    0100 0101  1111 1111  
+//              ---128---    ---10----    ---69----  ---255---
+//   Bytes:     1000 0000    0000 1010    0100 0101  1111 1111
 //   Base 64:   100000   000000   101001    000101   111111   110000
 //   Encoded: 01100000 01000000 01101001  01000101 01111111 01110000
 //            ---96--- ---64--- --105---  ---69--- --127--- --112---
@@ -38,14 +38,14 @@
 // </pre>
 // <para/>
 // An interesting property of this base-64 encoding is that when it encodes
-// bytes between 63 and 126, those bytes appear unchanged at certain 
-// offsets (specifically the third, sixth, ninth, etc.) In this example, 
-// since the third byte is 'E' (69), it also appears as 'E' in the 
+// bytes between 63 and 126, those bytes appear unchanged at certain
+// offsets (specifically the third, sixth, ninth, etc.) In this example,
+// since the third byte is 'E' (69), it also appears as 'E' in the
 // output.
 // <para/>
-// When viewing BAIS strings, another thing to keep in mind is that 
-// runs of zeroes ('\0') will tend to appear as runs of `@` characters 
-// in the base 64 encoding, although a single zero is not always enough 
+// When viewing BAIS strings, another thing to keep in mind is that
+// runs of zeroes ('\0') will tend to appear as runs of `@` characters
+// in the base 64 encoding, although a single zero is not always enough
 // to make a `@` appear. Runs of 255 will tend to appear as runs of `?`.
 // <para/>
 // There are many ways to encode a given byte array as BAIS.
@@ -68,7 +68,7 @@ func (ioobe IndexOutOfBoundsError) Error() string {
 func Encode(ba *[]byte, allowControlCharacters bool) string {
 	bytes := *ba
 	arrayLength := len(bytes)
-	sb := make([]byte,0,arrayLength * 3)
+	sb := make([]byte, 0, arrayLength*3)
 	i := 0
 	var b byte
 	for i < arrayLength {
@@ -77,7 +77,7 @@ func Encode(ba *[]byte, allowControlCharacters bool) string {
 			sb = append(sb, b)
 		} else {
 			b, i = getByte(bytes, i)
-			sb = append(sb,byte('\b'))
+			sb = append(sb, byte('\b'))
 			for ; ; b, i = getByte(bytes, i) {
 				var accum int32
 				accum = int32(b)
@@ -86,29 +86,29 @@ func Encode(ba *[]byte, allowControlCharacters bool) string {
 					accum = (accum << 8) | int32(b)
 					if i < arrayLength {
 						b, i = getByte(bytes, i)
-						accum = accum << 8 | int32(b)
-						sb = append(sb,byte(encodeBase64Digit(accum >> 18)))
-						sb = append(sb, byte(encodeBase64Digit(accum >> 12)))
-						sb = append(sb, byte(encodeBase64Digit(accum >> 6)))
+						accum = accum<<8 | int32(b)
+						sb = append(sb, byte(encodeBase64Digit(accum>>18)))
+						sb = append(sb, byte(encodeBase64Digit(accum>>12)))
+						sb = append(sb, byte(encodeBase64Digit(accum>>6)))
 						sb = append(sb, byte(encodeBase64Digit(accum)))
 						if i >= arrayLength {
 							break
 						}
 					} else {
-						sb = append(sb,byte(encodeBase64Digit(accum >> 10)))
-						sb = append(sb,byte(encodeBase64Digit(accum >> 4)))
-						sb = append(sb,byte(encodeBase64Digit(accum << 2)))
+						sb = append(sb, byte(encodeBase64Digit(accum>>10)))
+						sb = append(sb, byte(encodeBase64Digit(accum>>4)))
+						sb = append(sb, byte(encodeBase64Digit(accum<<2)))
 						break
 					}
 				} else {
-					sb = append(sb,byte(encodeBase64Digit(accum >> 2)))
-					sb = append(sb,byte((encodeBase64Digit(accum << 4))))
+					sb = append(sb, byte(encodeBase64Digit(accum>>2)))
+					sb = append(sb, byte((encodeBase64Digit(accum << 4))))
 					break
 				}
 				if isAscii(bytes, i, allowControlCharacters) &&
 					(i+1 >= len(bytes) || isAscii(bytes, i, allowControlCharacters)) &&
 					(i+2 >= len(bytes) || isAscii(bytes, i, allowControlCharacters)) {
-					sb = append(sb,byte('!'))
+					sb = append(sb, byte('!'))
 					break
 				}
 			}
@@ -122,7 +122,7 @@ func encodeBase64Digit(b int32) int32 {
 }
 
 func Decode(s string) []byte {
-	bb := make([]byte,0,len(s))
+	bb := make([]byte, 0, len(s))
 	sb := []byte(s)
 	for i := 0; i < len(sb); i++ {
 		if sb[i] == '\b' {
@@ -134,7 +134,7 @@ func Decode(s string) []byte {
 				}
 				digit := (cur - 64) & 63
 				zeros := 16 - 6
-				accum :=  digit << zeros
+				accum := digit << zeros
 				i++
 				for i < len(sb) {
 					if i >= len(sb) {
@@ -146,25 +146,22 @@ func Decode(s string) []byte {
 					}
 					digit = (cur - 64) & 63
 					zeros = zeros - 6
-					accum = accum | digit << zeros
+					accum = accum | digit<<zeros
 					if zeros <= 8 {
-						bb = append(bb,byte(accum >> 8))
+						bb = append(bb, byte(accum>>8))
 						accum = accum << 8
 						zeros = zeros + 8
 					}
 					i++
 				}
 
-				if (accum & 0xFF00) != 0 || (i < len(sb) && sb[i] != '!') {
+				if (accum&0xFF00) != 0 || (i < len(sb) && sb[i] != '!') {
 					panic(fmt.Errorf("%s can not be interpreted as a BAIS array", s))
 				}
 				i++
 
 				for i < len(sb) && sb[i] != '\b' {
 					bb = append(bb, sb[i])
-					if i >= len(sb) {
-						return bb[0:]
-					}
 					i++
 				}
 				if i >= len(sb) {
--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"fmt"
-	"bais/encoding"
+	"github.com/jarrodhroberson/bais/encoding/bais"
 )
 
 func main() {
 	arr := []byte{67, 97, 116, 128, 10, 69, 255, 65, 66, 67, 68}
 	fmt.Println(string(arr[:]))
 	//str := "Cat\b`@iE?tEB!CD"
-	str2 := encoding.ByteArrayInString(&arr,false)
+	str2 := bais.Encode(&arr, false)
 	fmt.Println(string(str2[:]))
 }
